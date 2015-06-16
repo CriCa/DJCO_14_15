3820347f10453a8a8cddd45c98cefead10b07ce0
Xkynar <xkynar@gmail.com>
2015-06-16 19:56:14 +0100

    Visual Update

---
+++ Assets/Scripts/Props/FlickeringController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringController : MonoBehaviour {
+
+	public float amplitude = 1f;
+	public float frequency = 0.5f;
+
+	private Light light;
+	private Color originalColor;
+
+	void Start () {
+		light = GetComponent<Light>(); 
+		originalColor = light.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = Time.time * frequency;
+		x = x - Mathf.Floor (x); //normalize
+
+		float y = 1 - (Random.value * 2);
+
+		float oscilation = y * amplitude + 1f;
+
+		light.color = originalColor * oscilation;
+	}
+}
---
+++ Assets/Scripts/Props/TVScreenController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TVScreenController : MonoBehaviour {
+
+	private float speed = 0.5f;
+	private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Random.Range (0, Time.time * speed);
+		renderer.material.SetTextureOffset ("_MainTex", new Vector2 (offset, 0));
+	}
+}
