6c327867e9e2dc5c67af2386b8eb184aa26036e0
Cristiano Carvalheiro <ei10041@fe.up.pt>
2015-06-18 20:19:56 +0100

    pause menu ingame, not working

---
+++ Assets/Scripts/MainMenu/PauseMenuController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuController : MonoBehaviour {
+
+	public void Resume() {
+
+	}
+
+	public void Disconnect() {
+		PhotonNetwork.Disconnect ();
+		PhotonNetwork.LoadLevel (0);
+	}
+
+	public void Exit() {
+		Application.Quit ();
+	}
+}
