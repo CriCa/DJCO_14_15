942ce353b0d570e75d131d641e28775315fd2457
Vasco Gomes <ovascogomes@gmail.com>
2015-06-18 20:44:54 +0100

    Added new "catch" type of room

---
+++ Assets/Scripts/Monsters/MonsterAttackController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (BoxCollider))]
+public class MonsterAttackController : MonoBehaviour 
+{
+	public float damage = 2f;
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			other.gameObject.GetComponent<PlayerNetworkManager>().TakeDamage(damage);
+		}
+	}
+}
