ecb606bbb5a33ef28f4283b281d35de89e3f9afb
Vasco Gomes <ovascogomes@gmail.com>
2015-06-18 15:13:41 +0100

    Updated StatuesRoom script

--- Assets/Scripts/Rooms/StatuesRoomController.cs
+++ Assets/Scripts/Rooms/StatuesRoomController.cs
@@ -9,43 +9,56 @@ using System.Collections.Generic;
 public class StatuesRoomController : MonoBehaviour 
 {
 	public float secondsToTrigger = 0f;
+	public float lookAroundInterval = 3f;
 	public GameObject monster;
 	public float monsterFOV = 45f;
 
-	bool triggered;
-	List<Transform> playersTr; // reference to players currently inside the room
-	List<CharacterController> playersCC; // not worth creating a custrom structure to hold both values
+	private bool triggered;
+	private bool isPlayerInside;
+	private Transform playerTransform;
+	private CharacterController playerControls;
 
 	int timesLooked = 0; // testing purposes only
 
+
 	void Start() {
-		playersTr = new List<Transform>();
-		playersCC = new List<CharacterController>();
 		triggered = false;
+		isPlayerInside = false;
 	}
-	
+
+
 	void OnTriggerEnter(Collider other) {
 		// using "Player", and not "PlayerBody", because we want to access the charactercontroller script on the parent object
 		if (other.tag == "Player") {
-			playersTr.Add(other.gameObject.transform);
-			playersCC.Add(other.gameObject.GetComponent<CharacterController>());
-
 			// when the first player enters, start process
 			if (!triggered) {
 				triggered = true;
-				InvokeRepeating("LookAround", secondsToTrigger, 3f);
+				InvokeRepeating("LookAround", secondsToTrigger, lookAroundInterval);
+			}
+
+			// if our own player entered the room
+			if (!isPlayerInside && other.gameObject == NetworkManager.instance.GetPlayer()) {
+				isPlayerInside = true;
+				playerTransform = other.gameObject.transform;
+				playerControls = other.gameObject.GetComponent<CharacterController>();
+
 			}
 		}
 	}
 
+
 	void LookAround() {
-		for (int i = 0; i < playersTr.Count; i++) {
-			if (IsPlayerMoving(playersTr[i], playersCC[i])) {
-				Debug.Log("Player is within field of view.");
-			}
-			else {
-				Debug.Log("Player can't be seen.");
-			}
+		// if local player is not inside, no need to check anything
+		if (!isPlayerInside) {
+			return;
+		}
+
+		// otherwise, we need to check if player is moving within fov
+		if (IsPlayerMoving(playerTransform, playerControls)) {
+			ChatManager.instance.AddMessage("Player is within field of view.");
+		}
+		else {
+			ChatManager.instance.AddMessage("Player can't be seen.");
 		}
 
 		timesLooked++;
@@ -56,6 +69,8 @@ public class StatuesRoomController : MonoBehaviour
 		}
 	}
 
+
+	// since a character controller is being used to check for movement, calculations can only be done locally (as in, each player is responsible for himself)
 	bool IsPlayerMoving(Transform playerTr, CharacterController playerCC) {
 		var direction = playerTr.position - monster.transform.position;
 		float angle = Vector3.Angle(direction, monster.transform.forward);
